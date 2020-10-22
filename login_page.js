087eaaab55b7d24f65c08843665dcb2420136576
Aina Ulain <quu865@uregina.ca>
2020-10-22 16:10:20 -0600

    add all files

---
+++ login_page.js
@@ -0,0 +1,17 @@
+function signUpFunction() {
+    var x = document.getElementById("signup");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function loginFunction() {
+    var x = document.getElementById("login");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
